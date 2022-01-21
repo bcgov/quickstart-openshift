7de5ed04a609f51dbb6c358ad7335e040e6ffb39
Derek Roberts <derek.roberts@gmail.com>
2022-01-20 17:13:13 -0800

    +eslint, @typescript-eslint

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
-import { AppModule } from './../src/app.module';
 import { INestApplication } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,10 +15,8 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+  it('/ (GET)', () => request(app.getHttpServer())
+    .get('/')
+    .expect(200)
+    .expect('Hello World!'));
 });
