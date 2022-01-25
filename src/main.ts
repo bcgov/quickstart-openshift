295782011a1d8966d815a077eddac3aafd911eec
Derek Roberts <derek.roberts@gmail.com>
2022-01-25 14:30:38 -0800

    Follow linting demands (now in PR) (#94)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function bootstrap () {
+  const app = await NestFactory.create(AppModule)
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
 
 // Junk commit
