bb63deb791a270aafa94279e079b1bd835259ac1
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2022-02-10 17:47:58 -0800

    code changes to get API running with 1. Easy connection to Postgres DB. 2. Automated DB schema migration through liquibase. 3. Swagger with OpenAPI 3 specification 4. Liveness/Readiness probes for kubernetes/openshift 5. Hibernate ORM with transaction, connection pooling etc.
    Future work is
    1. Integrate with keycloak to secure endpoints.
    2. Test multiple data sources(Oracle,Postgres) at the same time.
    3. Deploy through Git Actions to openshift.

---
+++ apis/Java/Quarkus/src/main/java/ca/bc/gov/nrs/api/v1/endpoint/EmployeeEndpoint.java
@@ -0,0 +1,37 @@
+package ca.bc.gov.nrs.api.v1.endpoint;
+
+import ca.bc.gov.nrs.api.v1.model.EmployeeEntity;
+
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.util.UUID;
+
+@Path("/api/v1/employee")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class EmployeeEndpoint {
+
+  @GET
+  @Path("/{id}")
+  public Response getEmployeeByID(@PathParam("id") UUID id) {
+    EmployeeEntity entity = EmployeeEntity.findById(id);
+    if (entity == null) {
+      return Response.status(Response.Status.NOT_FOUND).build();
+    } else {
+      return Response.ok(entity).build();
+    }
+  }
+
+  @POST
+  @Transactional
+  public Response createEmployee(EmployeeEntity employeeEntity) {
+    if(employeeEntity.getEmployeeId() != null) {
+      return Response.status(Response.Status.BAD_REQUEST).entity("{\"message\":\" employee Id is not allowed in POST.\"}").build();
+    }
+    employeeEntity.persist();
+    return Response.created(URI.create("/api/v1/employee" + employeeEntity.getEmployeeId())).build();
+  }
+}
