19c8c4c07a659d87d010e9342c33d4987f14ff00
Derek Roberts <derek.roberts@gmail.com>
2022-03-21 20:26:49 -0700

    Revert "code changes to get API running with (#132)"
    This reverts commit 6ebd3fac55cfe4a47d4763f21aa37d677b80f729.

--- apis/TypeScript/Nest/src/v1/modules/user.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from '../entities/user.entity';
-
-@Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  exports: [TypeOrmModule],
-})
-export class UsersModule {}
