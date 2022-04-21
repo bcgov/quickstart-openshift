113b54038726f89a99fd732b74fe83c7dc56434e
Derek Roberts <derek.roberts@gmail.com>
2022-04-20 17:00:03 -0700

    Use consistent vars for Postgres (#256)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,13 +6,13 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { UsersModule } from "./users/users.module";
 
-console.log("Var check - PG_HOST", process.env.PG_HOST);
-console.log("Var check - PG_DATABASE", process.env.PG_DATABASE);
-console.log("Var check - PG_USER", process.env.PG_USER);
-if (process.env.PG_PASSWORD != null ){
-  console.log("Var check - PG_PASSWORD present");
+console.log("Var check - POSTGRESQL_HOST", process.env.POSTGRESQL_HOST);
+console.log("Var check - POSTGRESQL_DATABASE", process.env.POSTGRESQL_DATABASE);
+console.log("Var check - POSTGRESQL_USER", process.env.POSTGRESQL_USER);
+if (process.env.POSTGRESQL_PASSWORD != null ){
+  console.log("Var check - POSTGRESQL_PASSWORD present");
 } else {
-  console.log("Var check - PG_PASSWORD not present");
+  console.log("Var check - POSTGRESQL_PASSWORD not present");
 }
 
 @Module({
@@ -20,11 +20,11 @@ if (process.env.PG_PASSWORD != null ){
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: "postgres",
-      host: process.env.PG_HOST || "localhost",
+      host: process.env.POSTGRESQL_HOST || "localhost",
       port: 5432,
-      database: process.env.PG_DATABASE || "postgres",
-      username: process.env.PG_USER || "postgres",
-      password: process.env.PG_PASSWORD,
+      database: process.env.POSTGRESQL_DATABASE || "postgres",
+      username: process.env.POSTGRESQL_USER || "postgres",
+      password: process.env.POSTGRESQL_PASSWORD,
       // entities: [User],
       autoLoadEntities: true, // Auto load all entities regiestered by typeorm forFeature method.
       synchronize: true, // This changes the DB schema to match changes to entities, which we might not want.
