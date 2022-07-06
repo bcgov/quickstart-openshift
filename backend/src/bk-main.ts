30bc29f6ce7cb0887cacd80df226ece2cda6f952
Derek Roberts <derek.roberts@gmail.com>
2022-07-06 14:07:08 -0700

    Copied junk to trigger pipeline

---
+++ backend/src/bk-main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle("Users example")
+    .setDescription("The user API description")
+    .setVersion("1.0")
+    .addTag("users")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
+
+// Junk commit
