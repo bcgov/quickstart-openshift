742293de6241ea40e69cfbfc8409ff0a257cd8c3
Derek Roberts <derek.roberts@gmail.com>
2022-08-12 14:09:21 -0700

    Trigger

--- frontend/src/app.service.ts
+++ frontend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common'
 @Injectable()
 export class AppService {
   getHello (): string {
-    return 'Hello Frontend Modification (x2)!'
+    return 'Hello Frontend Modification!'
   }
 }
