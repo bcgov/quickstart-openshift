411499ca126b2794ed8a98f2c413df86a04ba81d
Derek Roberts <derek.roberts@gmail.com>
2022-08-12 12:39:29 -0700

    Update workflow runners from ubuntu 18.04 to 20.04 (#441)

--- frontend/test/app.e2e-spec.ts
+++ frontend/test/app.e2e-spec.ts
@@ -18,5 +18,5 @@ describe('AppController (e2e)', () => {
   it('/ (GET)', () => request(app.getHttpServer())
     .get('/')
     .expect(200)
-    .expect('Hello Frontend Modification!'))
+    .expect('Hello Frontend Modification (x2)!'))
 })
