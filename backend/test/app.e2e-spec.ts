0fcc5ae89e28d0efbf63a5102a2f67568ab6b758
Derek Roberts <derek.roberts@gmail.com>
2022-08-17 16:50:37 -0700

    Triggers

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -18,5 +18,5 @@ describe('AppController (e2e)', () => {
   it('/ (GET)', () => request(app.getHttpServer())
     .get('/')
     .expect(200)
-    .expect('Hello Backend Modification #3!'))
+    .expect('Hello Backend!'))
 })
