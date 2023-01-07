f4a3103f9a4beb5aa49fc09b6aaa5ca6bc15830d
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2023-01-06 16:36:13 -0800

    Adding trust proxy to backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,14 +4,16 @@ import { AppModule } from "./app.module";
 import {customLogger} from "./common/logger.config";
 import helmet from 'helmet';
 import {Logger} from "@nestjs/common";
+import {NestExpressApplication} from "@nestjs/platform-express";
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     logger: customLogger,
   });
   app.use(
     helmet({ contentSecurityPolicy: false, crossOriginEmbedderPolicy: false, }),
   );
   app.enableCors();
+  app.set("trust proxy", 1);
   app.enableShutdownHooks();
   const config = new DocumentBuilder()
     .setTitle("Users example")
