cf316bfdfbb69206f22d4d7746a822d52514166d
Derek Roberts <derek.roberts@gmail.com>
2023-04-04 12:48:25 -0700

    Test

--- test.sh
+++ test.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-set -eux
-recipients="derek.roberts@gmail.com"
-subject="Test"
-from="derek.roberts@gov.bc.ca"
-/usr/sbin/sendmail -t "$recipients" <<EOF
-subject:$subject
-from:$from
+# set -eux
+# recipients="derek.roberts@gmail.com"
+# subject="Test"
+# from="derek.roberts@gov.bc.ca"
+# /usr/sbin/sendmail -t "$recipients" <<EOF
+# subject:$subject
+# from:$from
 
-Example Message
-EOF
+# Example Message
+# EOF
+
+
+subject="mail subject"
+body="Hello World"
+from="derek.roberts@gmail.com"
+to="derek.roberts@gmail.com"
+echo -e "Subject:${subject}\n${body}" | sendmail -f "${from}" -t "${to}"
