9ed89da17099abf1d3e3bfa4ec82a0b31097ee41
Derek Roberts <derek.roberts@gmail.com>
2023-04-22 09:53:33 -0700

    Test

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# set -eux
+set -eux
 # recipients="derek.roberts@gmail.com"
 # subject="Test"
 # from="derek.roberts@gov.bc.ca"
@@ -11,8 +11,17 @@
 # EOF
 
 
-subject="mail subject"
-body="Hello World"
-from="derek.roberts@gmail.com"
-to="derek.roberts@gmail.com"
-echo -e "Subject:${subject}\n${body}" | sendmail -f "${from}" -t "${to}"
+# subject="mail subject"
+# body="Hello World"
+# from="derek.roberts@gov.bc.ca"
+# to="derek.roberts@gov.bc.ca"
+# echo -e "Subject:${subject}\n${body}" | sendmail -f "${from}" -t "${to}"
+
+
+# curl --url 'smtps://apps.smtp.gov.bc.ca:25' --ssl-reqd \
+#   --mail-from 'derek.roberts@gmail.com' --mail-rcpt 'derek.roberts@gmail.com' \
+#   --upload-file mail.txt --user 'derek.roberts@gmail.com:your-accout-password'
+
+curl --url 'smtps://apps.smtp.extest.gov.bc.ca:25' --ssl-reqd \
+  --mail-from 'derek.roberts@gmail.com' --mail-rcpt 'derek.roberts@gmail.com' \
+  --upload-file mail.txt --user 'derek.roberts@gmail.com:your-accout-password'
