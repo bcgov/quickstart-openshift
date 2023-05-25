b8ff1e04d5f884464b681cf5cb492f09ae6a4c26
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2023-05-25 09:49:56 -0700

    fix the unit tests

--- backend-python/run-unit-tests.sh
+++ backend-python/run-unit-tests.sh
@@ -3,7 +3,8 @@ apt update
 curl -sSL https://install.python-poetry.org | python3 -
 export PATH="$POETRY_HOME/bin:$VENV_PATH/bin:$PATH"
 poetry install --no-root -vvv
-cd app || exit
 DATE_TIME=$(date +'%Y-%m-%d %H:%M:%S.%3N')
 echo "Starting unit tests at $DATE_TIME"
-ptw -v -w /application/app/test
+poetry run coverage run --source=app -m pytest app/test -x -o log_cli=true --disable-warnings -vvv
+poetry run coverage report
+poetry run coverage xml -o coverage-reports/coverage-report.xml
--- backend-python/test/test_main.py
+++ backend-python/test/test_main.py
@@ -1,17 +1,6 @@
-import os
-
-from src.main import get_logging_level, app
+from src.main import app
 
 
 def test_root():
     assert app is not None
 
-
-def test_get_logging_level():
-    os.environ["LOG_LEVEL"] = "40"
-    assert get_logging_level() == 40
-
-
-def test_get_logging_level_default():
-    del os.environ["LOG_LEVEL"]
-    assert get_logging_level() == 10
