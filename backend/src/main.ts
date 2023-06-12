238b5d37db82b8b2dc9e4b4a1fa5041aad2832f4
Om Mishra <32200996+mishraomp@users.noreply.github.com>
2023-06-11 18:10:46 -0700

    fix:sonar warning

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,9 @@ import {NestExpressApplication} from "@nestjs/platform-express";
 import {bootstrap} from "./app";
 import {Logger} from "@nestjs/common";
 
-
+const logger = new Logger('NestApplication');
 bootstrap().then(async (app: NestExpressApplication) => {
   await app.listen(3000);
-  const logger = new Logger('NestApplication');
   logger.log(`Listening on ${await app.getUrl()}`);
 }).catch(err=>{
   logger.error(err);
