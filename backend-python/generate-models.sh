0be6cce8b5f7fc178b6ad72320c62b899334c058
Derek Roberts <derek.roberts@gmail.com>
2023-06-20 15:36:52 -0700

    Finish up python

--- backend-python/generate-models.sh
+++ backend-python/generate-models.sh
@@ -3,4 +3,4 @@ python3 -m venv venv-py
 source venv-py/bin/activate
 pip install --upgrade pip
 pip install sqlalchemy sqlacodegen psycopg2-binary sqlacodegen[citext]
-sqlacodegen --schema py_api postgresql://postgres:postgres@database-py:5432/postgres > /application/models/model.py
+sqlacodegen --schema py_api postgresql://postgres:postgres@database:5432/postgres > /application/models/model.py
