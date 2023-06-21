0fe80e511dd7de2802c35bdae5c4b864fd481d71
Derek Roberts <derek.roberts@gmail.com>
2023-06-20 20:04:16 -0700

    Cleanup

--- backend-python/run-unit-tests.sh
+++ backend-python/run-unit-tests.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-apt update
+
+# Change to script dir
+cd $(dirname ${BASH_SOURCE[0]})
+
+# Setup poetry and install dependencies
 curl -sSL https://install.python-poetry.org | python3 -
 export PATH="$POETRY_HOME/bin:$VENV_PATH/bin:$PATH"
 poetry install --no-root -vvv
-DATE_TIME=$(date +'%Y-%m-%d %H:%M:%S.%3N')
-echo "Starting unit tests at $DATE_TIME"
-poetry run coverage run --source=app -m pytest app/test -x -o log_cli=true --disable-warnings -vvv
+
+# Start app
+echo "Starting unit tests at $(date +'%Y-%m-%d %H:%M:%S.%3N')"
+poetry run coverage run --source=app -m pytest ./test -x -o log_cli=true --disable-warnings -vvv
 poetry run coverage report
 poetry run coverage xml -o coverage-reports/coverage-report.xml
