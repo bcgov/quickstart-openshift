77de20fe94555431ca66668bc6b3c61a91e32fe7
Derek Roberts <derek.roberts@gmail.com>
2023-09-08 00:44:50 -0400

    Working with python:slim-bullseye

--- backend-python/start-openshift.sh
+++ backend-python/start-openshift.sh
@@ -2,4 +2,4 @@
 DATE_TIME=$(date +'%Y-%m-%d %H:%M:%S.%3N')
 cd app || exit
 echo "Starting uvicorn at $DATE_TIME"
-uvicorn src.main:app --host 0.0.0.0 --port 3000 --workers 1 --server-header --date-header --limit-concurrency 1000 --log-config /opt/logger.conf
+uvicorn src.main:app --host 0.0.0.0 --port 3000 --workers 1 --server-header --date-header --limit-concurrency 1000 --log-config ./logger.conf
