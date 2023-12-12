c50f2d171138d2409616e0d640fab872de82409d
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2023-12-12 09:41:00 -0800

    feat: Load TEST

---
+++ load-test/backend-test.js
@@ -0,0 +1,34 @@
+import { check } from "k6";
+import * as http from "k6/http";
+import { Rate } from "k6/metrics";
+
+
+export let errorRate = new Rate("errors");
+
+
+function checkStatus(response, checkName, statusCode = 200) {
+  let success = check(response, {
+    [checkName]: (r) => {
+      if (r.status === statusCode) {
+        return true;
+      } else {
+        console.error(checkName + " failed. Incorrect response code." + r.status);
+        return false;
+      }
+    }
+  });
+  errorRate.add(!success, { tag1: checkName });
+}
+
+
+export default function(token) {
+  let url = `${__ENV.BACKEND_URL}/v1/users`;
+  let params = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+  let res = http.get(url, params);
+  checkStatus(res, "get-all-users", 200);
+
+}
