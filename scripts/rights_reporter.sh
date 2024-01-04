63dd265efeb235a827a61fa8b5ff0b0ebb393087
Derek Roberts <derek.roberts@gmail.com>
2024-01-03 23:07:42 -0800

    Error handling for projects with insufficent rights

--- scripts/rights_reporter.sh
+++ scripts/rights_reporter.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 # This script reports on rights in OpenShift projects
+echo -e "OpenShift users for projects accessible to $(oc whoami)\n"
 
 # Get the current user's projects
 PROJECTS=$(oc projects | grep -v "*" | grep -E "^ +.*-.*(.*)$")
@@ -12,10 +13,13 @@ EXCLUDES="admin|legacy-access-github|ministry-viewer|openshift-pipelines-edit|pl
 
 # Loop through the projects and report on rights
 for p in $(echo "${PROJECTS}" | awk '{print $1}'); do
-  echo -e "\n---\n\nProject: $p"
-  echo -e "Name: $(echo "${PROJECTS}" | grep $p | awk -F" - " '{print $2}')"
+  echo -e "---\n\nProject: $p"
+  echo -e "Name: $(echo "${PROJECTS}" | grep $p | awk -F" - " '{print $2}')\n"
+
+  oc get rolebindings -n $p > /dev/null || continue
   for role in ${ROLES}; do
-    echo -e "\n${role}:"
+    echo -e "${role}:"
     oc get rolebindings -n $p -o json | jq -r '.items[] | select(.subjects[].kind=="User", .roleRef.name=="${role}") | .subjects[].name' | sort | uniq | sed "s/^/  /g" || echo "Insufficient rights for $p"
+    echo
   done
 done
