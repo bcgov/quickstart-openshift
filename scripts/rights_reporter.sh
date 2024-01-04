4920325cb983c0de942ae0c4301b40fc80819811
Derek Roberts <derek.roberts@gmail.com>
2024-01-03 21:01:55 -0800

    Greatly improve scrubbing

--- scripts/rights_reporter.sh
+++ scripts/rights_reporter.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 
 # Get the current user's projects
 PROJECTS=$(oc projects | grep -v "*" | grep -E "^ +.*-.*(.*)$")
+ROLES="admin edit view"
 
 # Upstream accounts to exclude
 EXCLUDES="admin|legacy-access-github|ministry-viewer|openshift-pipelines-edit|platform-services-controlled-admin|platform-services-controlled-helper|admin-[0-9]*|edit-[0-9]*"
@@ -12,6 +13,9 @@ EXCLUDES="admin|legacy-access-github|ministry-viewer|openshift-pipelines-edit|pl
 # Loop through the projects and report on rights
 for p in $(echo "${PROJECTS}" | awk '{print $1}'); do
   echo -e "\n---\n\nProject: $p"
-  echo -e "Name: $(echo "${PROJECTS}" | grep $p | awk -F" - " '{print $2}')\n"
-  oc get rolebindings -n $p | sort | grep -Ev "^(${EXCLUDES}) " | grep -E "ClusterRole/(admin|edit|view)" || echo "Insufficient rights for $p"
+  echo -e "Name: $(echo "${PROJECTS}" | grep $p | awk -F" - " '{print $2}')"
+  for role in ${ROLES}; do
+    echo -e "\n${role}:"
+    oc get rolebindings -n $p -o json | jq -r '.items[] | select(.subjects[].kind=="User", .roleRef.name=="${role}") | .subjects[].name' | sort | uniq | sed "s/^/  /g" || echo "Insufficient rights for $p"
+  done
 done
