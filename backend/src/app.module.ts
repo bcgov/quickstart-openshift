a81854a41101228a09382a44a7de75c3b8d507d6
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2024-02-16 12:29:54 -0800

    remove logger
    Signed-off-by: OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -17,8 +17,6 @@ const DB_PORT = process.env.POSTGRES_PORT || 5432;
 const DB_NAME = process.env.POSTGRES_DATABASE || "postgres";
 const DB_SCHEMA = process.env.DB_SCHEMA || "users";
 const dataSourceURL = `postgresql://${DB_USER}:${DB_PWD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?schema=${DB_SCHEMA}&connection_limit=5`;
-const logger = new Logger("HTTP");
-logger.log(`dataSourceURL: ${dataSourceURL}`);
 function getMiddlewares() {
   if (process.env.PRISMA_LOGGING) {
     return [
