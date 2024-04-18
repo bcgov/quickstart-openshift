635a5ab7dadc79e82266214f8207319fc7a03695
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2024-04-18 11:39:07 -0700

    revert the baseURL
    Signed-off-by: OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: 'https://quickstart-openshift-test-frontend.apps.silver.devops.gov.bc.ca/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
