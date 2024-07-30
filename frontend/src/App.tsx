a738a02c9a36bbead5950b4a06306c2d2b5a6acf
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2024-07-29 18:53:38 -0700

    remove components
    Signed-off-by: OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import {
   Footer,
   Header,
 } from "@bcgov/design-system-react-components";
+import { IconButton } from '~/@mui/material'
+import { HomeRounded } from '~/@mui/icons-material'
 
 const styles = {
   container: {
@@ -27,7 +29,11 @@ const styles = {
 export default function App() {
   return (
     <Box sx={styles.container}>
-      <Header />
+      <Header title={'QuickStart OpenShift'}> <a href={'/'} target={'_self'}>
+        <IconButton color="secondary">
+          <HomeRounded color="secondary"></HomeRounded>
+        </IconButton>
+      </a></Header>
       <Box sx={styles.content}>
         <BrowserRouter>
           <AppRoutes />
