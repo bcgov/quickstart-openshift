7c7332e647c816e8c043d6f9de8562befa6ac360
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2025-01-06 15:38:12 -0800

    chore: remove health check queries being printed to console.

--- backend/src/prisma.service.ts
+++ backend/src/prisma.service.ts
@@ -35,6 +35,8 @@ class PrismaService extends PrismaClient<Prisma.PrismaClientOptions, 'query'> im
   async onModuleInit() {
     await this.$connect();
     this.$on<any>('query', (e: Prisma.QueryEvent) => {
+      // dont print the health check queries
+      if(e?.query?.includes("SELECT 1")) return;
       this.logger.log(
         `Query: ${e.query} - Params: ${e.params} - Duration: ${e.duration}ms`,
       );
